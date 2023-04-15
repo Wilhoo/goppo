949055bf864f99e6e32228fba1d5b5a903bfc4c4
Adriano <adrianodelima785@hotmail.com>
2023-04-15 15:20:19 -0300

    Correção tipagem do Header

--- goppo/src/components/Header/HeaderInterface.tsx
+++ goppo/src/components/Header/HeaderInterface.tsx
@@ -1,52 +1,57 @@
-import {HeaderProps, HeaderItensType, Header} from '../Header/Header';
+import {HeaderItensType} from '../Header/Header';
 
 export const headerItens: HeaderItensType = {
+  id: 1,
   imageSrc: 'src/assets/logo.png',
-  headerItens: [
-    {
-      id: 1,
-      headerItemName: 'Máquinas de Passadoria',
-    },
+  headerItensX: {
+    headerItens: [
+      {
+        id: 1,
+        headerItemName: 'Máquinas de Passadoria',
+      },
 
-    {
-      id: 2,
-      headerItemName: 'Prensas Térmicas',
-    },
+      {
+        id: 2,
+        headerItemName: 'Prensas Térmicas',
+      },
 
-    {
-      id: 3,
-      headerItemName: 'Estação Tira Manchas',
-    },
+      {
+        id: 3,
+        headerItemName: 'Estação Tira Manchas',
+      },
 
-    {
-      id: 4,
-      headerItemName: 'Revisadores',
-    },
+      {
+        id: 4,
+        headerItemName: 'Revisadores',
+      },
 
-    {
-      id: 5,
-      headerItemName: 'Peças e Acessórios',
-    },
-  ],
-  headerSuperior: [
-    {
-      id: 1,
-      headerItemName: 'Página Inicial',
-    },
+      {
+        id: 5,
+        headerItemName: 'Peças e Acessórios',
+      },
+    ],
+    headerSuperior: [
+      {
+        id: 1,
+        headerItemName: 'Página Inicial',
+      },
 
-    {
-      id: 2,
-      headerItemName: 'Empresa',
-    },
+      {
+        id: 2,
+        headerItemName: 'Empresa',
+      },
 
-    {
-      id: 3,
-      headerItemName: 'Representantes',
-    },
+      {
+        id: 3,
+        headerItemName: 'Representantes',
+      },
 
-    {
-      id: 4,
-      headerItemName: 'Contato',
-    },
-  ],
+      {
+        id: 4,
+        headerItemName: 'Contato',
+      },
+    ],
+    imageSrc: 'src/assets/logo.png',
+  },
+  headerItemName: 'header',
 };
